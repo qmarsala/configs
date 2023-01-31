63f4f6cff4ab0e2ea77ce32e9ba9b9468cc3dd8c
Quinten Marsala <quinten.marsala@gmail.com>
2023-01-30 20:47:35 -0800

    apps

--- windows/apps.ps1
+++ windows/apps.ps1
@@ -7,13 +7,11 @@ Block "Configure Edge" {
     Set-RegistryValue "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name HubsSidebarEnabled -Value 0
 }
 
-if (!(Configured $forKids)) {
-    FirstRunBlock "Connect phone" {
-        Write-ManualStep "Connect phone"
-        start ms-phone:
-        ConfigureNotifications Microsoft.YourPhone_8wekyb3d8bbwe!YourPhoneMessages
-    }
-}
+# FirstRunBlock "Connect phone" {
+#     Write-ManualStep "Connect phone"
+#     start ms-phone:
+#     ConfigureNotifications Microsoft.YourPhone_8wekyb3d8bbwe!YourPhoneMessages
+# }
 
 UninstallBlock Microsoft.549981C3F5F10_8wekyb3d8bbwe # Cortana
 UninstallBlock Microsoft.BingNews_8wekyb3d8bbwe
@@ -29,12 +27,4 @@ UninstallBlock Microsoft.WindowsMaps_8wekyb3d8bbwe
 UninstallBlock Disney.37853FC22B2CE_6rarf9sa4v8jt # Disney+
 UninstallBlock SpotifyAB.SpotifyMusic_zpdnekdrzrea0
 UninstallBlock C27EB4BA.DropboxOEM_xbfy0k16fey96 # Dropbox promotion
-
-if (Configured $forKids) {
-    UninstallBlock MicrosoftTeams_8wekyb3d8bbwe
-}
-else {
-    Block "Teams > Settings > General > Auto-start Teams = Off" {
-        Set-RegistryValue "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\SystemAppData\MicrosoftTeams_8wekyb3d8bbwe\TeamsStartupTask" State 0
-    }
-}
+UninstallBlock MicrosoftTeams_8wekyb3d8bbwe
