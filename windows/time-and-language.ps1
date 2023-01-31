bcaf0ff876a8067ee366b4b949195fd4cd416b19
Quinten Marsala <quinten.marsala@gmail.com>
2023-01-30 20:40:43 -0800

    set time and lang for pst

--- windows/time-and-language.ps1
+++ windows/time-and-language.ps1
@@ -1,16 +1,3 @@
-Block "Time & language > Date & time > Time zone = (UTC-07:00) Mountain Time (US & Canada)" {
-    Set-TimeZone "Mountain Standard Time"
-}
-Block "Time & language > Date & time > Additional clocks" {
-    function SetAdditionalClock([int]$ClockNumber, [string]$DisplayName, [string]$TimeZoneId) {
-        Set-RegistryValue "HKCU:\Control Panel\TimeDate\AdditionalClocks\$ClockNumber" -Name Enable -Value 1
-        Set-RegistryValue "HKCU:\Control Panel\TimeDate\AdditionalClocks\$ClockNumber" -Name DisplayName -Value $DisplayName
-        Set-RegistryValue "HKCU:\Control Panel\TimeDate\AdditionalClocks\$ClockNumber" -Name TzRegKeyName -Value $TimeZoneId
-    }
-    SetAdditionalClock 1 "UTC" "UTC"
-    SetAdditionalClock 2 "Korea" "Korea Standard Time"
-}
-Block "Time & language > Typing > Advanced keyboard settings > Input language hot keys > Between input languages > Change Key Sequence > Switch Input Language = Not Assigned" {
-    Set-RegistryValue "HKCU:\Keyboard Layout\Toggle" -Name "Language Hotkey" -Value "3"
-    Set-RegistryValue "HKCU:\Keyboard Layout\Toggle" -Name "Layout Hotkey" -Value "3"
+Block "Time & language > Date & time > Time zone = (UTC-08:00) Pacific Time (US & Canada)" {
+    Set-TimeZone "Pacific Standard Time"
 }
